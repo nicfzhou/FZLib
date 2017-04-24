69f120a4bfe934c9e37270a95b193a92dd63dfd5
nicfzhou <nicfzhou@gmail.com>
2017-04-24 09:17:03 +0800

    初次建立

---
+++ FZLibDemo/FZLibDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FZLibDemo
+//
+//  Created by 周峰 on 2017/4/24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FZLibDemo/FZLibDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FZLibDemo
+//
+//  Created by 周峰 on 2017/4/24.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
