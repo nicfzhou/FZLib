f3371a8859d43452f1d4bb1f9851a4175b43f4d5
nicfzhou <nicfzhou@gmail.com>
2017-05-06 18:21:27 +0800

    demo 更新

--- FZLibDemo/FZLibDemo/ViewController.m
+++ FZLibDemo/FZLibDemo/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 #import "FZAutoLayout.h"
+#import "FZPageView.h"
+#import "ReactiveCocoa.h"
 
-@interface ViewController ()
+@interface ViewController ()<FZPageViewDelegate>
 
 @end
 
@@ -19,65 +21,38 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIView *view = [self randomView];
+    FZPageView *view = [[FZPageView alloc] init];
     [self.view addSubview:view];
-    view.fz_autoLayout.topEqualTo(self.view,FZAutoLayoutMaskTop).offset(20)
-    .leftEqualTo(self.view,FZAutoLayoutMaskLeft).offset(20)
-    .widthIs(100)
-    .aspectRatio(1/2.);
+    view.fz_autoLayout.edgeInsets(20,0,0,0);
     
+    view.delegate = self;
+    [view addViewController:[self randomVC] forTitle:@"标题标题1"];
+    [view addViewController:[self randomVC] forTitle:@"标题2"];
+    [view addViewController:[self randomVC] forTitle:@"标题标题标题3"];
+    [view addViewController:[self randomVC] forTitle:@"标题4"];
+    [view addViewController:[self randomVC] forTitle:@"t5"];
+    [view addViewController:[self randomVC] forTitle:@"t6"];
+    [view addViewController:[self randomVC] forTitle:@"t7"];
     
-    UIView *view2 = [self randomView];
-    [self.view addSubview:view2];
-    view2.fz_autoLayout.topEqualTo(view,FZAutoLayoutMaskBottom).offset(20)
-    .leftEqualTo(view,FZAutoLayoutMaskLeft)
-    .widthIs(100)
-    .heightIs(50);
+    view.style.titleSelectedColor = [UIColor redColor];
+    view.style.adjustTitleWidth = NO;
+    view.style.titleNormalFont = [UIFont boldSystemFontOfSize:15];
+    view.style.partingLineColor = [UIColor blackColor];
+    NSLog(@"%d %d",view.style.isAdjustTitleWidth,view.style.isEnabledScrollSwitch);
     
-    UIView *view3 = [self randomView];
-    [self.view addSubview:view3];
-    view3.fz_autoLayout.topEqualTo(view2,FZAutoLayoutMaskTop)
-    .leftEqualTo(view2,FZAutoLayoutMaskRight).offset(20)
-    .widthEqualTo(view2,FZAutoLayoutMaskWidth).multiplier(.5)
-    .heightEqualTo(view2,FZAutoLayoutMaskHeight).multiplier(2);
+    @weakify(view)
     
-    UIView *view4 = [self randomView];
-    [view addSubview:view4];
-    view4.fz_autoLayout.topEqualTo(view,FZAutoLayoutMaskTop).offset(10)
-    .leftEqualTo(view,FZAutoLayoutMaskLeft)
-    .aspectRatio(1/2.)
-    .aspectRatio(1/3.)
-    .bottomEqualTo(view,FZAutoLayoutMaskBottom);
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(20, 100, 100, 44);
+    btn.backgroundColor = [UIColor redColor];
+    [btn setTitle:@" 删除 " forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+    [[btn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(UIButton *x){
+//        @strongify(view);
+        [view removeFromSuperview];
+        [x removeFromSuperview];
     
-    
-    UIView *view5 = [self randomView];
-    [view addSubview:view5];
-    view5.fz_autoLayout.edgeInsets(UIEdgeInsetsMake(10, 10, 10, 10));
-    
-    UILabel *label = [[UILabel alloc] init];
-    label.text = @"是打发大大师傅阿斯蒂芬阿斯蒂芬撒旦法沙发阿斯蒂芬暗室逢灯阿凡达阿道夫按时";
-    [self.view addSubview:label];
-    label.fz_autoLayout.topEqualTo(view4,FZAutoLayoutMaskBottom).offset(10)
-    .leftEqualTo(view4,FZAutoLayoutMaskLeft);
-    
-    UILabel *label2 = [[UILabel alloc] init];
-    label2.text = @"是打发大大师傅阿斯蒂芬阿斯蒂芬撒旦法沙发阿斯蒂芬暗室逢灯阿凡达阿道夫按时";
-    label2.numberOfLines = 0;
-    [self.view addSubview:label2];
-    label2.fz_autoLayout.topEqualTo(label,FZAutoLayoutMaskBottom).offset(10)
-    .leftEqualTo(label,FZAutoLayoutMaskLeft)
-    .widthIs(100)
-    .autoHeight();
-    
-    UILabel *label3 = [[UILabel alloc] init];
-    label3.text = @"是打发大大师傅阿斯蒂芬阿斯蒂芬撒旦法沙发阿斯蒂芬暗室逢灯阿凡达阿道夫按时";
-    label3.numberOfLines = 0;
-    [self.view addSubview:label3];
-    label3.fz_autoLayout.topEqualTo(label2,FZAutoLayoutMaskBottom).offset(10)
-    .leftEqualTo(label,FZAutoLayoutMaskLeft)
-    .autoWidth()
-    .heightIs(100);
-    
+    }];
 }
 
 
@@ -90,6 +65,7 @@
 - (UIView *)randomView{
     UIView *view = [[UIView alloc] init];
     view.backgroundColor = [self randomColor];
+    [self.view addSubview:view];
     return view;
 }
 
@@ -97,4 +73,14 @@
     return [UIColor colorWithRed:(arc4random()%256)/255. green:(arc4random()%256)/255. blue:(arc4random()%256)/255. alpha:1];
 }
 
+
+- (UIViewController *)randomVC{
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [self randomColor];
+    return vc;
+}
+
+- (void)pageView:(FZPageView *)pageView pageChanged:(NSInteger)page viewController:(UIViewController *)viewController title:(NSString *)title{
+    NSLog(@"%@ - %@",title,@(page));
+}
 @end
